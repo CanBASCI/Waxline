28ab8594c303742058550c4a9d0e5c2a1b8d9ad3
Can Basci <can.basci@fups.com>
2026-09-01 16:42:02 +0300

    Initial Commit

---
+++ Waxline/WaxlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaxlineApp.swift
+//  Waxline
+//
+//  Created by Can Basci on 1.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct WaxlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
